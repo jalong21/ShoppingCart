cb00ab1215b5ee36fd8f7c1f089c89e185bb9bb5
Austin <jeffreyaustinlong@gmail.com>
2022-05-23 21:13:47 -0400

    adding external methods

--- app/controllers/CartController.scala
+++ app/controllers/CartController.scala
@@ -22,14 +22,72 @@ class CartController @Inject()(cc: ControllerComponents,
   implicit val askTimeout = Timeout(5.seconds)
 
   def createCart(name: String, state: Option[String]) = Action {
-    request => request.body.asJson
-        .map( requestBody => {
-          log.warn(s"createCart: ${requestBody.toString()}")
-          val cartID = ask(cartActor, CreateCart(name, state))
-            .mapTo[String]
-          Ok(Await.result[String](cartID, 5.seconds))
-        })
-      .getOrElse(BadRequest("Payload Missing From Request"))
+    log.warn(s"createCart - name: $name state: $state")
+    val cartID = ask(cartActor, CreateCart(name, state))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
   }
 
+  def updateCart(id: String, state: Option[String]) = Action {
+    log.warn(s"updateCart - id: $id state: $state")
+    val cartID = ask(cartActor, CreateCart(id, state))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def deleteCart(id: String) = Action {
+    log.warn(s"deleteCart - id: $id")
+    val cartID = ask(cartActor, CreateCart(id, None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def getCart(id: String) = Action {
+    log.warn(s"getCart - id: $id")
+    val cartID = ask(cartActor, CreateCart(id, None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def getItems() = Action {
+    log.warn(s"getItems")
+    val cartID = ask(cartActor, CreateCart("id", None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def addItemToCart(itemID: Int, cartID: String) = Action {
+    log.warn(s"addItemToCart - item: $itemID, cartID: $cartID")
+    val cartID = ask(cartActor, CreateCart("id", None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def removeItemFromCart(itemID: Int, cartID: String) = Action {
+    log.warn(s"removeItemFromCart - item: $itemID, cartID: $cartID")
+    val cartID = ask(cartActor, CreateCart("id", None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def emptyCart(cartID: String) = Action {
+    log.warn(s"emptyCart - cartID: $cartID")
+    val cartID = ask(cartActor, CreateCart("id", None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def applyCouponToCart(couponID: String, cartID: String) = Action {
+    log.warn(s"applyCouponToCart - cartID: $cartID, couponID: $couponID")
+    val cartID = ask(cartActor, CreateCart("id", None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
+
+  def checkOutCart(paymentId: String, cartID: String) = Action {
+    log.warn(s"checkOutCart - cartID: $cartID, paymentId: $paymentId")
+    val cartID = ask(cartActor, CreateCart("id", None))
+      .mapTo[String]
+    Ok(Await.result[String](cartID, 5.seconds))
+  }
 }
