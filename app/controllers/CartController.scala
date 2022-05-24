4ec00de7fef92f048cd9fb0232807d340106e740
Austin <jeffreyaustinlong@gmail.com>
2022-05-23 20:44:46 -0400

    adding basic actor to start

--- app/controllers/CartController.scala
+++ app/controllers/CartController.scala
@@ -3,17 +3,14 @@ package controllers
 import akka.actor.ActorSystem
 import akka.pattern.ask
 import akka.util.Timeout
-import play.api.{Configuration, Logger}
 import play.api.mvc._
+import play.api.{Configuration, Logger}
 import services.CartActor
 import services.CartActor.CreateCart
 
-import java.util.UUID
 import javax.inject.Inject
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import scala.util.{Failure, Success, Try}
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class CartController @Inject()(cc: ControllerComponents,
                                conf: Configuration) extends AbstractController(cc) {
@@ -22,7 +19,7 @@ class CartController @Inject()(cc: ControllerComponents,
 
   val system = ActorSystem("CartActorSystem")
   val cartActor = system.actorOf(CartActor.props(conf), name = "CartActor")
-  implicit val askTimeout = Timeout(5 seconds)
+  implicit val askTimeout = Timeout(5.seconds)
 
   def createCart(name: String, state: Option[String]) = Action {
     request => request.body.asJson
@@ -30,7 +27,7 @@ class CartController @Inject()(cc: ControllerComponents,
           log.warn(s"createCart: ${requestBody.toString()}")
           val cartID = ask(cartActor, CreateCart(name, state))
             .mapTo[String]
-          Ok(Await.result[String](cartID, 5 seconds))
+          Ok(Await.result[String](cartID, 5.seconds))
         })
       .getOrElse(BadRequest("Payload Missing From Request"))
   }
